7032a33afbd4b1efe674c73b84ce6ad406a3a136
rosto-infinity <rostoinfocus@gmail.com>
2025-12-19 21:43:12 +0100

    -Update the user's profile information.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -24,7 +24,8 @@ class ProfileController extends Controller
     }
 
     /**
-     * Update the user's profile information.
+     * -Update the user's profile information.
+     * 
      */
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
